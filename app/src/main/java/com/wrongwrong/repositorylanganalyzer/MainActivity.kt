8cea1bb2f6d553d11992da7aad896c98c30981ea
bntwng <boranti1995@gmail.com>
2018-07-17 04:15:02 +0900

    ネットワークを使うには非同期処理で動かす必要があるができていない https://qiita.com/k-kagurazaka@github/items/702c92bc3381af36db12 https://qiita.com/kawmra/items/ee4acb7db61f70dec9a8

--- app/src/main/java/com/wrongwrong/repositorylanganalyzer/MainActivity.kt
+++ app/src/main/java/com/wrongwrong/repositorylanganalyzer/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.wrongwrong.repositorylanganalyzer
 
+import android.content.Context
+import android.net.ConnectivityManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.NetworkOnMainThreadException
 import android.widget.Button
 import android.widget.Toast
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.launch
+import kotlinx.coroutines.experimental.runBlocking
 import java.io.FileNotFoundException
 import java.net.URL
 
@@ -13,12 +19,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var b =findViewById<Button>(R.id.launchButton)
+        var b: Button = this.findViewById(R.id.launchButton)
         b.setOnClickListener {
+            Toast.makeText(this, "開始", Toast.LENGTH_LONG).show()
             try{
-                GitHubUtil.getJsonFromURL(URL("https://api.github.com/users/k163377/repos"))
+                b.isEnabled = false
+                var a = async{
+                    GitHubUtil.getJsonFromURL(URL("https://api.github.com/users/k163377/repos"))
+                }.await()
+                Toast.makeText(this, "正常終了", Toast.LENGTH_LONG).show()
             }catch (e: FileNotFoundException){
-                Toast.makeText(this, "リポジトリを見つけられませんでした。ユーザー名が正しいか確認してください。", Toast.LENGTH_SHORT)
+                Toast.makeText(this, "リポジトリを見つけられませんでした。ユーザー名が正しいか確認してください。", Toast.LENGTH_SHORT).show()
+            }catch (e: NetworkOnMainThreadException){
+                Toast.makeText(this, "ネットワークに接続できませんでした。", Toast.LENGTH_SHORT).show()
+            }finally {
+                b.isEnabled = true
             }
         }
 
