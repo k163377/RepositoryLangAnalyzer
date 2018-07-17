13c4be6f37a43fcd2d02fdf228cbc42e56307649
bntwng <boranti1995@gmail.com>
2018-07-17 12:31:47 +0900

    try-catchのロジックがおかしかったので修正

--- app/src/main/java/com/wrongwrong/repositorylanganalyzer/MainActivity.kt
+++ app/src/main/java/com/wrongwrong/repositorylanganalyzer/MainActivity.kt
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.NetworkOnMainThreadException
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import kotlinx.coroutines.experimental.android.UI
 import kotlinx.coroutines.experimental.launch
@@ -16,24 +17,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var b: Button = this.findViewById(R.id.launchButton)
+        var input = findViewById<EditText>(R.id.inputAccount)
+        var b = findViewById<Button>(R.id.launchButton)
         b.setOnClickListener {
             Toast.makeText(this, "開始", Toast.LENGTH_LONG).show()
-            try{
-                var context = this
-                b.isEnabled = false
-                val launch = launch(UI) {
-                    var json = GitHubUtil.getJsonFromURL(URL("https://api.github.com/users/k163377/repos"))
+            var context = this
+            b.isEnabled = false
+            val launch = launch(UI) {
+                try {
+                    var json = GitHubUtil.getJsonFromURL(URL("https://api.github.com/users/${input.text}/repos"))
                     Toast.makeText(context, json.await(), Toast.LENGTH_LONG).show()
+                } catch (e: FileNotFoundException) {
+                    Toast.makeText(context, "リポジトリを見つけられませんでした。ユーザー名が正しいか確認してください。", Toast.LENGTH_SHORT).show()
+                } catch (e: NetworkOnMainThreadException) {
+                    Toast.makeText(context, "ネットワークに接続できませんでした。", Toast.LENGTH_SHORT).show()
+                } finally {
+                    b.isEnabled = true
                 }
-            }catch (e: FileNotFoundException){
-                Toast.makeText(this, "リポジトリを見つけられませんでした。ユーザー名が正しいか確認してください。", Toast.LENGTH_SHORT).show()
-            }catch (e: NetworkOnMainThreadException){
-                Toast.makeText(this, "ネットワークに接続できませんでした。", Toast.LENGTH_SHORT).show()
-            }finally {
-                b.isEnabled = true
             }
         }
-
     }
 }
