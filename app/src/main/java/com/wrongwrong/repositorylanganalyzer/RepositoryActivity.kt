fb5465698439c638a504d991ab3a00aacd3145b2
bntwng <boranti1995@gmail.com>
2018-07-17 23:32:32 +0900

    リポジトリ名はタイトルに表示するように変更

--- app/src/main/java/com/wrongwrong/repositorylanganalyzer/RepositoryActivity.kt
+++ app/src/main/java/com/wrongwrong/repositorylanganalyzer/RepositoryActivity.kt
@@ -1,6 +1,10 @@
 package com.wrongwrong.repositorylanganalyzer
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.widget.ListView
 import android.widget.TextView
@@ -12,9 +16,11 @@ class RepositoryActivity: AppCompatActivity() {
         setContentView(R.layout.activity_repository)
 
         var titleLang = intent.getStringExtra("language")
-        findViewById<TextView>(R.id.title_language).text = titleLang
         var lv = findViewById<ListView>(R.id.repository_list)
         lv.adapter = RepositoryAdapter(this, titleLang, intent.extras["repositories"] as ArrayList<String>,
                 intent.extras["languages"] as ArrayList<String>, intent.extras["descriptions"] as ArrayList<String>)
+
+        title = titleLang
+        supportActionBar!!.setBackgroundDrawable(ColorDrawable(ContextCompat.getColor(this, R.color.colorAccent)))
     }
 }
