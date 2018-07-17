fe594889127222d185065c4036f45e4d5a2c8701
bntwng <boranti1995@gmail.com>
2018-07-17 13:09:48 +0900

    アダプター作成

---
+++ app/src/main/java/com/wrongwrong/repositorylanganalyzer/LanguageAdapter.kt
@@ -0,0 +1,34 @@
+package com.wrongwrong.repositorylanganalyzer
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+
+class LanguageAdapter(context: Context, sortedList: List<Pair<String, Int>>) : BaseAdapter() {
+    var cont = context
+    var layoutInflater = cont.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+    var list = sortedList
+
+    override fun getCount(): Int {
+        return list.count()
+    }
+
+    override fun getItem(position: Int): Pair<String, Int> {
+        return list[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        var view = layoutInflater.inflate(R.layout.language_item, parent, false)
+        view.findViewById<TextView>(R.id.num).setText("${list[position].first}")
+        view.findViewById<TextView>(R.id.language).setText(list[position].second)
+
+        return view
+    }
+}
