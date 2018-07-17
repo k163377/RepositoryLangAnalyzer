c8fc3140216732f25ce6e8e1531c99747770c05b
bntwng <boranti1995@gmail.com>
2018-07-18 00:04:06 +0900

    コメント化部を削除

--- app/src/main/java/com/wrongwrong/repositorylanganalyzer/GitHubUtil.kt
+++ app/src/main/java/com/wrongwrong/repositorylanganalyzer/GitHubUtil.kt
@@ -39,61 +39,6 @@ class GitHubUtil {
             return array
         }
 
-        /*// リポジトリ一覧をJSONから取得
-        fun getRepsFromJSON(json: String): ArrayList<String>{
-            val full_name = "full_name\":\""
-
-            val split = json.split("\",\"")
-            val regex = Regex("$full_name.+")
-            val reps = ArrayList<String>()
-            var result: MatchResult?
-
-            for(str in split){
-                result = regex.find(str)
-                if(result != null) reps.add(result.value.substring(full_name.length))
-            }
-            return reps
-        }
-
-        //jsonからdescriptionの項を取得
-        fun getDescriptionFromJSON(json: String): ArrayList<String>{
-            var description = "description\":"
-
-            var split = json.split(",\"")
-            var regex = Regex("$description.+")
-            var descriptions = ArrayList<String>()
-            var temp: String
-
-            for(str in split){
-                var result = regex.find(str)
-                if(result != null){
-                    temp = result.value.substring(description.length)
-                    if(temp.last() == '"') temp = temp.substring(0, temp.length-1).substring(1)
-                    descriptions.add(temp)
-                }
-            }
-            return descriptions
-        }
-
-        //jsonから言語の項を取得
-        fun getLanguagesFromJSON(json: String): ArrayList<String>{
-            var language = "language\":"
-
-            var split = json.split(",\"")
-            var regex = Regex("$language.+")
-            var languages = ArrayList<String>()
-
-            for(str in split){
-                var result = regex.find(str)
-                if(result != null){
-                    var temp = result.value.substring(language.length)
-                    if(temp.equals("null")) languages.add("Other Language")
-                    else languages.add(temp.substring(0, temp.length-1).substring(1))
-                }
-            }
-            return languages
-        }*/
-
         //言語の項の列から、どの言語がどの順で多いかをソート済みリストとして返す
         fun makeRankOfLangs(langs:  ArrayList<String>): List<Pair<String, Int>>{
             var map = HashMap<String, Int>()
