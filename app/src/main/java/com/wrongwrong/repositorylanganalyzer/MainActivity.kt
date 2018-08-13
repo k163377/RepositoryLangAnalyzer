bc6e7f9df0d670ab6e38bfec4e1722c79c07f96d
bntwng <boranti1995@gmail.com>
2018-08-13 19:26:17 +0900

    有効/無効切り替えができていなかったパーツに切り替えを設定 ついでに若干動作が軽くなったはず

--- app/src/main/java/com/wrongwrong/repositorylanganalyzer/MainActivity.kt
+++ app/src/main/java/com/wrongwrong/repositorylanganalyzer/MainActivity.kt
@@ -20,44 +20,50 @@ import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
     var repositories: List<Repo>? = null
+    //パーツ
+    lateinit var launchButton: Button
+    lateinit var listView: ListView
+    lateinit var input: EditText
+
+    private fun setPartsEnabled(b: Boolean){
+        this.launchButton.isEnabled = b
+        this.listView.isEnabled = b
+        this.input.isEnabled = b
+    }
 
     //ボタン押したときの挙動設定
     private fun initButton(){
-        val input = findViewById<EditText>(R.id.inputAccount)
-        val b = findViewById<Button>(R.id.launchButton)
-        b.setOnClickListener {
+        launchButton.setOnClickListener {
             //キーボードの非表示
             val imm = (getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager)
             if (imm.isActive) imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0)
 
+            //パーツ類を無効化
+            setPartsEnabled(false)
+
             //処理開始通知のトースト表示
             Toast.makeText(this, "Start.", Toast.LENGTH_LONG).show()
             val context = this
-            //ボタンを無効化しjson取得など
-            b.isEnabled = false
 
-            val reposCall = getReposCall(input.text.toString())
+            val reposCall = getReposCall(input.text.toString()) //データを取得
             reposCall.enqueue(object : Callback<List<Repo>> {
                 override fun onResponse(call: Call<List<Repo>>?, response: Response<List<Repo>>?) {
                     try{
                         repositories = response!!.body()
                         if(repositories != null && repositories!!.isNotEmpty()){
                             val rankOfLangs = makeRankOfLangs(repositories)
-                            val lv = findViewById<ListView>(R.id.listView)
                             val numColors = ArrayList<Int>()
                             for(p in rankOfLangs){
                                 numColors.add(
                                         resources.getIdentifier(
                                                 p.first.replace(' ', '_')
-                                                .replace('+', '_')
-                                                .replace('#', '_')
-                                                .replace('-', '_')
-                                                .replace('\'', '_'),
+                                                .replace('+', '_').replace('#', '_')
+                                                .replace('-', '_').replace('\'', '_'),
                                                 "color", packageName
                                         )
                                 )
                             }
-                            lv.adapter = LanguageAdapter(context, rankOfLangs, repositories!!.count(), numColors)
+                            listView.adapter = LanguageAdapter(context, rankOfLangs, repositories!!.count(), numColors)
                             Toast.makeText(context, "Success", Toast.LENGTH_SHORT).show()
                         } else {
                             Toast.makeText(context, "Couldn't find any repositories.\n" +
@@ -66,13 +72,13 @@ class MainActivity : AppCompatActivity() {
                     }catch (e: IOException) {
                         Log.d("onResponse", "IOException")
                     }finally {
-                        b.isEnabled = true
+                        setPartsEnabled(true)
                     }
                 }
 
                 override fun onFailure(call: Call<List<Repo>>?, t: Throwable?) {
                     Toast.makeText(context, "Network communication failed.\nPlease check the communication status.", Toast.LENGTH_LONG).show()
-                    b.isEnabled = true
+                    setPartsEnabled(true)
                 }
             })
         }
@@ -82,11 +88,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        launchButton = findViewById(R.id.launchButton)
+        listView = findViewById(R.id.listView)
+        input = findViewById(R.id.inputAccount)
+
         supportActionBar!!.setBackgroundDrawable(ColorDrawable(ContextCompat.getColor(this, R.color.colorPrimaryDark)))
 
         initButton()
-        findViewById<ListView>(R.id.listView).setOnItemClickListener{parent, v, position, id ->
-            var intent = Intent(this.applicationContext, RepositoryActivity::class.java)
+        listView.setOnItemClickListener{parent, v, position, id ->
+            val intent = Intent(this.applicationContext, RepositoryActivity::class.java)
             intent.putExtra("language", (parent.getItemAtPosition(position) as Pair<String, Int>).first)
             intent.putExtra("repositories", repositories!!.toTypedArray())
             startActivity(intent)
