ab80e54e75fea42cd1e27b297c8a7edc51887053
Unknown <boranti1995@gmail.com>
2018-09-19 22:34:55 +0900

    kotlinx式で書き直し

--- app/src/main/java/com/wrongwrong/repositorylanganalyzer/RepositoryActivity.kt
+++ app/src/main/java/com/wrongwrong/repositorylanganalyzer/RepositoryActivity.kt
@@ -6,7 +6,7 @@ import android.net.Uri
 import android.os.Bundle
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
-import android.widget.ListView
+import kotlinx.android.synthetic.main.activity_repository.*
 
 class RepositoryActivity: AppCompatActivity() {
 
@@ -16,7 +16,6 @@ class RepositoryActivity: AppCompatActivity() {
 
         val titleLang = intent.getStringExtra("language")
         val repos = intent.extras["repositories"] as Array<Repo>
-        val lv = findViewById<ListView>(R.id.repository_list)
         val titleColor = resources.getIdentifier(
                 titleLang.replace(' ', '_')
                         .replace('+', '_')
@@ -25,13 +24,13 @@ class RepositoryActivity: AppCompatActivity() {
                         .replace('\'', '_'),
                 "color", packageName
         )
-        lv.adapter = RepositoryAdapter(this, titleLang, repos, if(titleColor == 0) R.color.colorWhite else titleColor)
+        repository_list.adapter = RepositoryAdapter(this, titleLang, repos, if(titleColor == 0) R.color.colorWhite else titleColor)
 
         //タイトルを変更
         title = titleLang
         supportActionBar!!.setBackgroundDrawable(ColorDrawable(ContextCompat.getColor(this, R.color.colorPrimaryDark)))
 
-        lv.setOnItemClickListener { parent, v, position, id ->
+        repository_list.setOnItemClickListener { parent, v, position, id ->
             val uri = Uri.parse((parent.getItemAtPosition(position) as Repo).html_url)
             val intent = Intent(Intent.ACTION_VIEW, uri)
             startActivity(intent)
